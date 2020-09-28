cb8a20fe595ab1c951752f0b107aec0afd82cd9e
Muhammad <muhammad.azam@dmdmax.com>
2020-09-28 18:49:33 +0500

    db connection check

--- repos/UserRepo.js
+++ repos/UserRepo.js
@@ -3,15 +3,17 @@ class UserRepository {
     async getUsersByDateRange (req, from, to) {
         console.log('getUsersByDateRange: ', from, to);
         req.db.collection('users', function (err, collection) {
-            console.log('collection====================================: ', collection);
 
             if (!err) {
                 console.log('yes: ');
-                return collection.find({}).toArray(function(err, items) {
+                return collection.find({
+                    $and:[{added_dtm:{$gte:new Date(from)}}, {added_dtm:{$lte:new Date(to)}}]
+                }).toArray(function(err, items) {
                     if(err){
                         console.log(err);
+                        return []
                     }
-                    console.log(items);
+                    return items;
                 });
             }
         });
