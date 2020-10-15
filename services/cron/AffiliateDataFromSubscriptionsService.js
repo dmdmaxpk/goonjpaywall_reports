c6e5fd9bff386514ae4128fa5e481f3d03be6086
Muhammad <muhammad.azam@dmdmax.com>
2020-10-15 16:56:54 +0500

    affiliate report - cron affiliate - package wise data

--- services/cron/AffiliateDataFromSubscriptionsService.js
+++ services/cron/AffiliateDataFromSubscriptionsService.js
@@ -13,7 +13,7 @@ computeAffiliateReports = async(req, res) => {
     * Compute date and time for data fetching from db
     * Script will execute to fetch data as per day
     * */
-    dateData = helper.computeNextDate(req, 7, 25);
+    dateData = helper.computeNextDate(req, 25, 7);
     req = dateData.req;
     day = dateData.day;
     month = dateData.month;
