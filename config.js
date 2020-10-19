594e63c5429e441aee288680a555489d156e07e7
Muhammad <muhammad.azam@dmdmax.com>
2020-10-19 15:28:11 +0500

    affiliate - cron - billing history data - query optimization

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || 'development';
 
-const cron_db_query_data_limit = 700000;
+const cron_db_query_data_limit = 800000;
 let config = {
     development: {
         port: '3006',
