a62b3463dc9764c5e7f6cf149ef89056dea9aa4a
Muhammad <muhammad.azam@dmdmax.com>
2020-10-19 14:05:24 +0500

    affiliate - cron - billing history data - query optimization

--- repos/cron/BillingHistoryRepo.js
+++ repos/cron/BillingHistoryRepo.js
@@ -1,4 +1,4 @@
-const config = require('../../config');
+const config = require('./../../config');
 class BillingHistoryRepository {
     async getBillingHistoryByDateRange (req, from, to) {
         return new Promise((resolve, reject) => {
