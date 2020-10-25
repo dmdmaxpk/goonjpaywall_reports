731c73eba6bf4e2877512dff7876a8e71988e491
Muhammad <muhammad.azam@dmdmax.com>
2020-10-25 12:47:40 +0000

    reports - cron - charge details cron is working

--- services/cron/ReportsCronService.js
+++ services/cron/ReportsCronService.js
@@ -23,49 +23,49 @@ cronComputeReports = async(req, res) => {
     console.log('UserService - computeUserReports **********************************************');
 
     // compute Subscribers report Data
-    await helper.sleep(1000 * 60 * 20);
+    await helper.sleep(1000 * 60 * 6);
     console.log('SubscriberService - computeSubscriberReports **********************************************');
     await SubscriberService.computeSubscriberReports(req,res);
 
     // compute Subscriptions report Data
-    await helper.sleep(1000 * 60 * 20);
+    await helper.sleep(1000 * 60 * 6);
     console.log('SubscriptionService - computeSubscriptionReports **********************************************');
     await SubscriptionService.computeSubscriptionReports(req,res);
 
     // compute Callback report Data
-    await helper.sleep(1000 * 60 * 20);
+    await helper.sleep(1000 * 60 * 6);
     // await CallbackSendService.computeCallbackSendReports(req,res);
 
     // compute Charge Details report Data
-    await helper.sleep(1000 * 60 * 20);
+    await helper.sleep(1000 * 60 * 6);
     // await SubscriptionBillingHistoryService.computeChargeDetailsReports(req,res);
 
     // compute Net Addition report Data
-    await helper.sleep(1000 * 60 * 20);
+    await helper.sleep(1000 * 60 * 6);
     // await RevenueNetAdditionService.computeRevenueNetAdditionReports(req,res);
 
     // compute Affiliate - Helogs report Data
-    await helper.sleep(1000 * 60 * 20);
+    await helper.sleep(1000 * 60 * 6);
     // await HelogsService.computeHelogsReports(req,res);
 
     // compute Affiliate - Helogs Unique Access report Data
-    await helper.sleep(1000 * 60 * 20);
+    await helper.sleep(1000 * 60 * 6);
     // await HelogsService.computeHelogsUniqueSuccessReports(req,res);
 
     // compute Affiliate - Page View Data
-    await helper.sleep(1000 * 60 * 20);
+    await helper.sleep(1000 * 60 * 6);
     // await LogsService.computeLogsPageViewReports(req,res);
 
     // compute Affiliate - Subscribe Clicks Data
-    await helper.sleep(1000 * 60 * 20);
+    await helper.sleep(1000 * 60 * 6);
     // await LogsService.computeLogsSubscribeClicksReports(req,res);
 
     // compute Affiliate - Affiliate Mids report Data
-    await helper.sleep(1000 * 60 * 20);
+    await helper.sleep(1000 * 60 * 6);
     // await AffiliateSubscriptionsService.computeAffiliateMidsFromSubscriptionsReports(req,res);
 
     // compute Affiliate - Affiliate report Data
-    await helper.sleep(1000 * 60 * 20);
+    await helper.sleep(1000 * 60 * 6);
     // await AffiliateSubscriptionsService.computeAffiliateReports(req,res);
 
 };
