f6f8294c978a614ae5e0a00877231b6370bdb22e
Muhammad <muhammad.azam@dmdmax.com>
2020-10-27 16:29:41 +0500

    reports - cron - affiliate report data improvements

--- middlewares/connection.js
+++ middlewares/connection.js
@@ -17,22 +17,24 @@ let connect = async (req, res, next) => {
 
 
 let updateConnection = async (req, res, next, connectType) => {
-    await MongoClient.connect(config.mongoDB[connectType], async function(err, client) {
-        if(err){
-            console.error(`Error: ${err.message}`);
-            res.status(403).send(connectType, "  - Database Access Denied");
-        }else{
+    return new Promise(async (resolve, reject) => {
+            await MongoClient.connect(config.mongoDB[connectType],  async function (err, client) {
+            if(err){
+                console.error(`Error: ${err.message}`);
+                res.status(403).send(connectType, "  - Database Access Denied");
+            }else{
 
-            console.log('DB name: ', connectType);
-            req.db = await client.db(connectType);
-            console.log('req.db: ', req.db);
+                console.log('DB name: ', connectType);
+                req.db = await client.db(connectType);
+                console.log('req.db: ', req.db);
 
-            await helper.setDBInstance(req.db);
-            if (next !== null)
-                next();
-            else
-                return true;
-        }
+                await helper.setDBInstance(req.db);
+                if (next !== null)
+                    resolve(next());
+                else
+                    resolve(true);
+            }
+        });
     });
 };
 
