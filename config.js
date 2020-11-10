8d0faa0b6acb56329d5fbcdc9cfce20ce07238f6
Muhammad <muhammad.azam@dmdmax.com>
2020-11-10 12:46:03 +0500

    cron - subscription optimizations

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || 'development';
 
-const cron_db_query_data_limit = 80000;
+const cron_db_query_data_limit = 50000;
 let config = {
     development: {
         port: '3006',
