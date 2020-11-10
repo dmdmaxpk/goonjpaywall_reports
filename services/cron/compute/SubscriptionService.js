287a48be19d872106a381290da2d36809ce09a54
Muhammad <muhammad.azam@dmdmax.com>
2020-11-10 14:23:04 +0500

    cron - subscription optimizations

--- services/cron/compute/SubscriptionService.js
+++ services/cron/compute/SubscriptionService.js
@@ -14,7 +14,7 @@ computeSubscriptionReports = async(req, res) => {
     * Compute date and time for data fetching from db
     * Script will execute to fetch data as per day
     * */
-    dateData = helper.computeNextDate(req, 15, 10);
+    dateData = helper.computeNextDate(req, 16, 10);
     req = dateData.req;
     day = dateData.day;
     month = dateData.month;
