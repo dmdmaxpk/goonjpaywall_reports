929f83e423ce42720e174647e5c7c9a96ecfa3da
Muhammad Azam <muhammad.azam@dmdmax.com>
2020-12-03 13:53:52 +0500

    avg. transactions per costumer - api  - is in progress

--- services/apis/compute/TransactionService.js
+++ services/apis/compute/TransactionService.js
@@ -30,7 +30,7 @@ computeAvgTransactionsSizeReport = async (rawDataSet, params) =>{
                         monthlyDataObj.weeklyComedy = monthlyDataObj.weeklyComedy + innerObj.weeklyComedy;
 
                     console.log('innerObj.month: ', innerObj.month);
-                    monthlyDataObj.month = innerObj.month;
+                    monthlyDataObj.month = packageObj.month;
                     monthWiseTotalCount.push(_.clone(monthlyDataObj));
                     monthlyDataObj = _.clone({dailyLive: 0, weeklyLive: 0, dailyComedy: 0, weeklyComedy: 0, month: ''});
                 }
@@ -68,8 +68,7 @@ computeAvgTransactionsPerCustomerReport = async (rawDataSet, params) =>{
                     if (innerObj.weeklyComedy)
                         monthlyDataObj.weeklyComedy = monthlyDataObj.weeklyComedy + innerObj.weeklyComedy;
 
-                    console.log('innerObj.month: ', innerObj.month);
-                    monthlyDataObj.month = innerObj.month;
+                    monthlyDataObj.month = packageObj.month;
                     monthWiseTotalCount.push(_.clone(monthlyDataObj));
                     monthlyDataObj = _.clone({dailyLive: 0, weeklyLive: 0, dailyComedy: 0, weeklyComedy: 0, month: ''});
                 }
