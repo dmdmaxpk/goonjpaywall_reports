cffb09602e35353db8084d00cb02631ff9b2b9cd
Muhammad Azam <muhammad.azam@dmdmax.com>
2021-01-03 16:04:28 +0500

    cron job - year set to 2021

--- helper/helper.js
+++ helper/helper.js
@@ -145,7 +145,7 @@ class Helper {
     static computeTodayDate(req){
         let date, fromDate, toDate, day, month;
         date = new Date();
-        date.setDate(date.getDate() - 1);
+        date.setDate(date.getDate() - 2);
 
         day =  date.getDate();
         day = day > 9 ? day : '0'+Number(day);
@@ -171,7 +171,7 @@ class Helper {
     static computeTodayDateWithLocalTime(req){
         let date, fromDate, toDate, day, month;
         date = new Date();
-        date.setDate(date.getDate() - 1);
+        date.setDate(date.getDate() - 2);
 
         day =  date.getDate();
         day = day > 9 ? day : '0'+Number(day);
