1149dfe36d88f78823b7dd1a9112128240b32a10
Muhammad Azam <muhammad.azam@dmdmax.com>
2021-02-07 13:03:41 +0500

    ccd - api data computation - is in progress

--- repos/cron/CcdApiDataRepo.js
+++ repos/cron/CcdApiDataRepo.js
@@ -23,7 +23,6 @@ class CcdApiDataRepo {
                                 added_dtm: { '$dateToString' : { date: "$added_dtm", 'timezone' : "Asia/Karachi" } },
                             }
                         },
-                        { $limit: 3 }
                     ],{ allowDiskUse: true }).toArray(function(err, items) {
                         if(err){
                             console.log('getDataFromLogger - err: ', err.message);
