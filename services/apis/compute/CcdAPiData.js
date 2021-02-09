915589dff7c0fc75e5cc7a31c6c3ade43e9037b8
Muhammad Azam <muhammad.azam@dmdmax.com>
2021-02-09 12:36:27 +0500

    ccd report - api call type - option is added

--- services/apis/compute/CcdAPiData.js
+++ services/apis/compute/CcdAPiData.js
@@ -28,6 +28,13 @@ getCcdApiData = async(req, res) => {
                 newObj.msisdn = history.req_body.msisdn;
 
                 if (history.res_body){
+                    let res_body = history.res_body;
+                    if (res_body.code)
+                        delete res_body.code;
+
+                    if (res_body.gw_transaction_id)
+                        delete res_body.gw_transaction_id;
+
                     newObj.api_service_response = JSON.stringify(history.res_body);
                     if (history.res_body.message === 'Requested subscriptions has unsubscribed!')
                         newObj.service_deactivation = 'Yes';
