0a3b62b4f8f04a3801606e73c7ffba29bb72b46e
Muhammad Azam <muhammad.azam@dmdmax.com>
2021-03-30 14:59:04 +0500

    request count  - api data support

--- services/apis/compute/StatisticsService.js
+++ services/apis/compute/StatisticsService.js
@@ -9,39 +9,39 @@ computeRequestCountReport = async (rawDataSet, params) =>{
 
     let monthNo, dayNo, week_from_date = null, month_from_date = null;
     let outerObj, innerObj, hourlyBasisTotalCount = [], dayWiseTotalCount = [], weekWiseTotalCount = [], monthWiseTotalCount = [];
-    let dataObj = {successful: 0, expired: 0, churn: 0};
-    let dayDataObj = {successful: 0, expired: 0, churn: 0};
-    let weeklyDataObj = {successful: 0, expired: 0, churn: 0};
-    let monthlyDataObj = {successful: 0, expired: 0, churn: 0};
+    let dataObj = {successful: 0, graced: 0, total: 0};
+    let dayDataObj = {successful: 0, graced: 0, total: 0};
+    let weeklyDataObj = {successful: 0, graced: 0, total: 0};
+    let monthlyDataObj = {successful: 0, graced: 0, total: 0};
     if (rawDataSet.length > 0){
         for (let i=0; i<rawDataSet.length; i++){
             outerObj = rawDataSet[i];
-            if (outerObj.churn){
-                for (let j=0; j<outerObj.churn.length; j++) {
-                    innerObj = outerObj.churn[j];
+            if (outerObj.requestCount){
+                for (let j=0; j<outerObj.requestCount.length; j++) {
+                    innerObj = outerObj.requestCount[j];
                     if (innerObj.success) {
                         dataObj.successful = dataObj.successful + innerObj.success;
                         dayDataObj.successful = dayDataObj.successful + innerObj.success;
                         weeklyDataObj.successful = weeklyDataObj.successful + innerObj.success;
                         monthlyDataObj.successful = monthlyDataObj.successful + innerObj.success;
                     }
-                    if (innerObj.expired) {
-                        dataObj.expired = dataObj.expired + innerObj.expired;
-                        dayDataObj.expired = dayDataObj.expired + innerObj.expired;
-                        weeklyDataObj.expired = weeklyDataObj.expired + innerObj.expired;
-                        monthlyDataObj.expired = monthlyDataObj.expired + innerObj.expired;
+                    if (innerObj.graced) {
+                        dataObj.graced = dataObj.graced + innerObj.graced;
+                        dayDataObj.graced = dayDataObj.graced + innerObj.graced;
+                        weeklyDataObj.graced = weeklyDataObj.graced + innerObj.graced;
+                        monthlyDataObj.graced = monthlyDataObj.graced + innerObj.graced;
                     }
-                    if (innerObj.churn) {
-                        dataObj.churn = dataObj.churn + innerObj.churn;
-                        dayDataObj.churn = dayDataObj.churn + innerObj.churn;
-                        weeklyDataObj.churn = weeklyDataObj.churn + innerObj.churn;
-                        monthlyDataObj.churn = monthlyDataObj.churn + innerObj.churn;
+                    if (innerObj.total) {
+                        dataObj.total = dataObj.total + innerObj.total;
+                        dayDataObj.total = dayDataObj.total + innerObj.total;
+                        weeklyDataObj.total = weeklyDataObj.total + innerObj.total;
+                        monthlyDataObj.total = monthlyDataObj.total + innerObj.total;
                     }
 
                     // Hourly Bases Data
                     // hourlyBasisTotalCount.push({
-                    //     successful: innerObj.success, expired: innerObj.expired,
-                    //     churn: innerObj.churn, date: outerObj.date
+                    //     successful: innerObj.success, graced: innerObj.graced,
+                    //     total: innerObj.total, date: outerObj.date
                     // });
 
                     // reset start_date for both month & week so can update with latest one
@@ -59,7 +59,7 @@ computeRequestCountReport = async (rawDataSet, params) =>{
                     monthlyDataObj.from_date = month_from_date;
                     monthlyDataObj.to_date = outerObj.date;
                     monthWiseTotalCount.push(_.clone(monthlyDataObj));
-                    monthlyDataObj = _.clone({successful: 0, expired: 0, churn: 0});
+                    monthlyDataObj = _.clone({successful: 0, graced: 0, total: 0});
                     month_from_date = null;
                 }
 
@@ -68,14 +68,14 @@ computeRequestCountReport = async (rawDataSet, params) =>{
                     weeklyDataObj.from_date = week_from_date;
                     weeklyDataObj.to_date = outerObj.date;
                     weekWiseTotalCount.push(_.clone(weeklyDataObj));
-                    weeklyDataObj = _.clone({successful: 0, expired: 0, churn: 0});
+                    weeklyDataObj = _.clone({successful: 0, graced: 0, total: 0});
                     week_from_date = null;
                 }
 
                 // Day Wise Date Count
                 dayDataObj.date = outerObj.date;
                 dayWiseTotalCount.push(_.clone(dayDataObj));
-                dayDataObj = _.clone({successful: 0, expired: 0, churn: 0});
+                dayDataObj = _.clone({successful: 0, graced: 0, total: 0});
             }
         }
 
