ef754727ed77ba348c6db6dae638bd1f9898a846
Muhammad Azam <muhammad.azam@dmdmax.com>
2021-09-02 16:07:35 +0500

    cron job - date setting for previous data computations

--- services/cron/compute/SubscriptionsSourceWiseSuccessfulService.js
+++ services/cron/compute/SubscriptionsSourceWiseSuccessfulService.js
@@ -16,7 +16,7 @@ SubscriptionsSourceWiseSuccessfulService = async(req, res) => {
     * Compute date and time for data fetching from db
     * Script will execute to fetch data as per day
     * */
-    dateData = helper.computeNextDateWithLocalTime(req, 15, 10);
+    dateData = helper.computeNextDateWithLocalTime(req, 30, 8);
     req = dateData.req;
     day = dateData.day;
     month = dateData.month;
