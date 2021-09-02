5676b4a6d2568b4c84c9b97cb5705bd3a6aece90
Muhammad Azam <muhammad.azam@dmdmax.com>
2021-09-02 14:13:15 +0500

    cron job - date setting for previous data computations

--- repos/cron/SubscriberRepo.js
+++ repos/cron/SubscriberRepo.js
@@ -3,14 +3,14 @@ class SubscriberRepository {
     async getSubscribersByDateRange (req, from, to) {
         return new Promise((resolve, reject) => {
             console.log('getSubscribersByDateRange: ', from, to);
-            req.db.collection('subscribers', function (err, collection) {
+            req.db.collection('users', function (err, collection) {
                 if (!err) {
                     collection.aggregate([
                         { $match: {
                             $and:[{added_dtm:{$gte:new Date(from)}}, {added_dtm:{$lte:new Date(to)}}]
                         }},
                         {$project: {
-                            user_id: "$user_id",
+                            user_id: "$_id",
                             added_dtm: { '$dateToString' : { date: "$added_dtm", 'timezone' : "Asia/Karachi" } },
                         }}
                     ],{ allowDiskUse: true }).toArray(function(err, items) {
